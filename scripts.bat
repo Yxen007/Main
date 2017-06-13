ea72fad1fced7860ed2dd5831d65788720463a3a
Yxen007 <hanz007@gmail.com>
2017-06-13 15:16:36 +0100

    Add files via upload

---
+++ scripts.bat
@@ -0,0 +1,2 @@
+@xcopy "C:\projects\EPLANP8\trunk\Scripts\ESBI_Master.cs" "C:\P8\Addin\" /y /c /q
+@xcopy "C:\projects\EPLANP8\trunk\Scripts\ESBI_WorkplaceSwap.cs" "W:\Addons\MainAddon\1.0.0\BIN\" /y /c /q
